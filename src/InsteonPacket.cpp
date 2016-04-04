f2ccea5a38d313fd045634b676ca6f31c4872c5f
Sathya Laufer <sathya@laufers.net>
2016-04-04 19:28:15 +0200

    Fixed static code analysis issues

--- src/InsteonPacket.cpp
+++ src/InsteonPacket.cpp
@@ -438,12 +438,12 @@ std::vector<uint8_t> InsteonPacket::getPosition(double index, double size, int32
 			uint32_t bitSize = std::lround(size * 10);
 			if(bitSize > 8) bitSize = 8;
 			uint32_t intIndex = std::lround(std::floor(index));
-			if(intIndex == 0) result.push_back(((_senderAddress >> 16) >> (std::lround(index * 10) % 10)) & _bitmask[bitSize]);
-			else if(intIndex == 1) result.push_back(((_senderAddress >> 8) >> (std::lround(index * 10) % 10)) & _bitmask[bitSize]);
-			else if(intIndex == 2) result.push_back((_senderAddress >> (std::lround(index * 10) % 10)) & _bitmask[bitSize]);
-			else if(intIndex == 3) result.push_back(((_destinationAddress >> 16) >> (std::lround(index * 10) % 10)) & _bitmask[bitSize]);
-			else if(intIndex == 4) result.push_back(((_destinationAddress >> 8) >> (std::lround(index * 10) % 10)) & _bitmask[bitSize]);
-			else if(intIndex == 5) result.push_back((_destinationAddress >> (std::lround(index * 10) % 10)) & _bitmask[bitSize]);
+			if(intIndex == 0) result.push_back(((_senderAddress >> 16) >> (std::lround(index * 10) % 8)) & _bitmask[bitSize]);
+			else if(intIndex == 1) result.push_back(((_senderAddress >> 8) >> (std::lround(index * 10) % 8)) & _bitmask[bitSize]);
+			else if(intIndex == 2) result.push_back((_senderAddress >> (std::lround(index * 10) % 8)) & _bitmask[bitSize]);
+			else if(intIndex == 3) result.push_back(((_destinationAddress >> 16) >> (std::lround(index * 10) % 8)) & _bitmask[bitSize]);
+			else if(intIndex == 4) result.push_back(((_destinationAddress >> 8) >> (std::lround(index * 10) % 8)) & _bitmask[bitSize]);
+			else if(intIndex == 5) result.push_back((_destinationAddress >> (std::lround(index * 10) % 8)) & _bitmask[bitSize]);
 			else if(intIndex == 6)
 			{
 				if(index >= 6.5) result.push_back((uint8_t)_flags & _bitmask[bitSize]);
@@ -451,8 +451,8 @@ std::vector<uint8_t> InsteonPacket::getPosition(double index, double size, int32
 				else if(index >= 6.2) result.push_back(_hopsLeft >> 2);
 				else result.push_back(_hopsMax);
 			}
-			else if(intIndex == 7) result.push_back((_messageType >> (std::lround(index * 10) % 10)) & _bitmask[bitSize]);
-			else if(intIndex == 8) result.push_back((_messageSubtype >> (std::lround(index * 10) % 10)) & _bitmask[bitSize]);
+			else if(intIndex == 7) result.push_back((_messageType >> (std::lround(index * 10) % 8)) & _bitmask[bitSize]);
+			else if(intIndex == 8) result.push_back((_messageSubtype >> (std::lround(index * 10) % 8)) & _bitmask[bitSize]);
 			return result;
 		}
 		index -= 9;
@@ -474,7 +474,7 @@ std::vector<uint8_t> InsteonPacket::getPosition(double index, double size, int32
 			//The round is necessary, because for example (uint32_t)(0.2 * 10) is 1
 			uint32_t bitSize = std::lround(size * 10);
 			if(bitSize > 8) bitSize = 8;
-			result.push_back((_payload.at(intByteIndex) >> (std::lround(index * 10) % 10)) & _bitmask[bitSize]);
+			result.push_back((_payload.at(intByteIndex) >> (std::lround(index * 10) % 8)) & _bitmask[bitSize]);
 		}
 		else
 		{
