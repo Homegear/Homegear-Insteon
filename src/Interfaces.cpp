0355ac3f81c5175ffd9f9e1bce820565bd8c4e9a
Sathya Laufer <sathya@laufers.net>
2016-07-14 14:02:57 +0200

    Changes to family and interface settings

--- src/Interfaces.cpp
+++ src/Interfaces.cpp
@@ -34,7 +34,7 @@
 namespace Insteon
 {
 
-Interfaces::Interfaces(BaseLib::Obj* bl, std::vector<std::shared_ptr<Systems::PhysicalInterfaceSettings>> physicalInterfaceSettings) : Systems::PhysicalInterfaces(bl, GD::family->getFamily(), physicalInterfaceSettings)
+Interfaces::Interfaces(BaseLib::Obj* bl, std::map<std::string, Systems::PPhysicalInterfaceSettings> physicalInterfaceSettings) : Systems::PhysicalInterfaces(bl, GD::family->getFamily(), physicalInterfaceSettings)
 {
 	create();
 }
@@ -48,19 +48,19 @@ void Interfaces::create()
 	try
 	{
 
-		for(std::vector<std::shared_ptr<Systems::PhysicalInterfaceSettings>>::iterator i = _physicalInterfaceSettings.begin(); i != _physicalInterfaceSettings.end(); ++i)
+		for(std::map<std::string, Systems::PPhysicalInterfaceSettings>::iterator i = _physicalInterfaceSettings.begin(); i != _physicalInterfaceSettings.end(); ++i)
 		{
 			std::shared_ptr<IInsteonInterface> device;
-			if(!*i) continue;
-			GD::out.printDebug("Debug: Creating physical device. Type defined in insteon.conf is: " + (*i)->type);
-			if((*i)->type == "insteonhubx10") device.reset(new InsteonHubX10(*i));
-			else GD::out.printError("Error: Unsupported physical device type: " + (*i)->type);
+			if(!i->second) continue;
+			GD::out.printDebug("Debug: Creating physical device. Type defined in insteon.conf is: " + i->second->type);
+			if(i->second->type == "insteonhubx10") device.reset(new InsteonHubX10(i->second));
+			else GD::out.printError("Error: Unsupported physical device type: " + i->second->type);
 			if(device)
 			{
-				if(_physicalInterfaces.find((*i)->id) != _physicalInterfaces.end()) GD::out.printError("Error: id used for two devices: " + (*i)->id);
-				_physicalInterfaces[(*i)->id] = device;
-				GD::physicalInterfaces[(*i)->id] = device;
-				if((*i)->isDefault || !GD::defaultPhysicalInterface) GD::defaultPhysicalInterface = device;
+				if(_physicalInterfaces.find(i->second->id) != _physicalInterfaces.end()) GD::out.printError("Error: id used for two devices: " + i->second->id);
+				_physicalInterfaces[i->second->id] = device;
+				GD::physicalInterfaces[i->second->id] = device;
+				if(i->second->isDefault || !GD::defaultPhysicalInterface) GD::defaultPhysicalInterface = device;
 			}
 		}
 	}
